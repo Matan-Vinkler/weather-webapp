c42686d9f942db6e469d143809bda966f93b64fe
Matan Vinkler <matanvinkler@gmail.com>
2021-03-10 23:17:51 +0200

    /

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ To run the code:
 
 1. "node src/app.js" or
 2. "nodemon src/app.js"
+
+Seriously, what did you think I would write in this "README" file? Do you recieve compensations from your country?
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded!")
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const location = search.value
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
 
-    fetch('/weather?addr=' + location).then((response) => {
+    fetch("/weather?addr=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
